1607b66bd4c2179cbcf1823cd009b6f25b6cff2b
Joao_Guilherme2 <80895578+JoaoG23@users.noreply.github.com>
2022-10-03 08:01:37 -0300

    Bug deploy corrigido com sucesso pasta de __tests__

--- __test__/integrations/index.test.ts
+++ __test__/integrations/index.test.ts
@@ -1,5 +1,5 @@
 import request from "supertest";
-import app from "../../app";
+import app from "../../src/app";
 import { it, describe, expect } from "vitest";
 import { destinario } from "../seeds";
 
